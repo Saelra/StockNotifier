140389a39ab190d3d61123bebafff8dc4b34a4ca
Justin Jemison <jjemi8884@gmail.com>
2025-03-03 05:13:27 -0800

    created state for graph fom dashboard

--- app/dashboard.tsx
+++ app/dashboard.tsx
@@ -3,14 +3,23 @@ import {View, Text, Button, Image, TouchableOpacity, TextInput, StyleSheet} from
 import {Link} from "expo-router";
 import DashBoardHeader from '@/components/DashBoardHeader';
 import BasicChart from '@/components/BasicChart';
+import {useState} from "react"
+
+
+
+const chartData = [100, 200, 300, 400, 500];
+const  chartLabels = ["1 hour", "2 hour", "3 hour", "4 hour", "5 hour"];
+
+
 
 
 const dashboard = () => {
-    return (
+  const[dataState, setDataState] = useState([100, 200, 300, 400, 500]);
+  return (
       <View>
         <DashBoardHeader/>
-        <Text>Symbol will go here</Text>
-        <BasicChart/>
+        <Text>Symbol will go here and some more</Text>
+        <BasicChart chartData={dataState} chartLabels={chartLabels}/>
 
       </View>
     );
--- components/BasicChart.tsx
+++ components/BasicChart.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Text, View } from "react-native";
-
 import {
     LineChart,
     BarChart,
@@ -12,23 +11,23 @@ import {
 import { Dimensions } from "react-native";
 const screenWidth = Dimensions.get("window").width;
 
-const ChartObject = () => {
+
+type chartProp = {
+  chartData : number[],
+  chartLabels: string[]  
+}
+
+const ChartObject = ({ chartData, chartLabels}: chartProp, dataS: dataState ) => {
     return (
     <View>
-    <Text>Bezier Line Chart</Text>
+    
+    
     <LineChart
         data={{
-        labels: ["January", "February", "March", "April", "May", "June"],
+        labels: chartLabels,
         datasets: [
             {
-            data: [
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100
-            ]
+            data: chartData
             }
         ]
         }}
@@ -38,9 +37,9 @@ const ChartObject = () => {
         yAxisSuffix="k"
         yAxisInterval={1} // optional, defaults to 1
         chartConfig={{
-        backgroundColor: "#e26a00",
-        backgroundGradientFrom: "#fb8c00",
-        backgroundGradientTo: "#ffa726",
+        backgroundColor: "#0cbcd2",
+        backgroundGradientFrom: "#12dcd7",
+        backgroundGradientTo: "#7812dc",
         decimalPlaces: 2, // optional, defaults to 2dp
         color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
         labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
@@ -53,55 +52,17 @@ const ChartObject = () => {
             stroke: "#ffa726"
         }
         }}
-        bezier
+  
         style={{
         marginVertical: 8,
         borderRadius: 16
         }}
         />
+       
         </View>
     )
 }
 
-const chartConfig = {
-    backgroundGradientFrom: "#1E2923",
-    backgroundGradientFromOpacity: 0,
-    backgroundGradientTo: "#08130D",
-    backgroundGradientToOpacity: 0.5,
-    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
-    strokeWidth: 2, // optional, default 3
-    barPercentage: 0.5,
-    useShadowColorFromDataset: false // optional
-  };
 
-  const data = {
-    labels: ["January", "February", "March", "April", "May", "June"],
-    datasets: [
-      {
-        data: [20, 45, 28, 80, 99, 43],
-        color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
-        strokeWidth: 2 // optional
-      }
-    ],
-    legend: ["Rainy Days"] // optional
-  };
 
-  <LineChart
-  data={data}
-  width={screenWidth}
-  height={220}
-  chartConfig={chartConfig}
-/>
-
-const chart = () => {
-    return (
-    <>
-    <Text>Hello from chart still here</Text>
-    <ChartObject />
-    <Text>Here to</Text>
-    
-    </>
-    );
-}
-
-export default chart;
+export default ChartObject;
